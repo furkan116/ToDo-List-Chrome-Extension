a567be7e4e933796caf9ccffb1c19abb5c5f49eb
Furkan Yıldırım <mfurkanyildirim116@gmail.com>
2021-08-16 13:49:40 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -8,38 +8,83 @@ var span = document.getElementsByClassName("close")[0];
 runExt();
 
 function runExt() {
+  document.addEventListener("DOMContentLoaded", loadAllToDosToUI);
   form.addEventListener("click", addTodo);
   todoList.addEventListener("click", deleteToDo);
+
 }
 
 function addTodo(e) {
   const newTodo = addTodoText.value.trim();
   addTodoToUI(newTodo);
+  addtodoToStorage(newTodo);
 
   e.preventDefault();
 }
 
 function addTodoToUI(newTodo) {
-    const newListItem = document.createElement("li");
-    const newSpanItem = document.createElement("span");
-    const newButtonItem = document.createElement("button");
-  
-    newListItem.className = "todo-list-item";
-    newButtonItem.id = "deleteButton";
-    newButtonItem.innerHTML = `<i class="fas fa-trash"></i>`;
-  
-    newSpanItem.appendChild(document.createTextNode(newTodo));
-    newListItem.appendChild(newSpanItem);
-    newListItem.appendChild(newButtonItem);
-  
-    todoList.appendChild(newListItem);
+  const newListItem = document.createElement("li");
+  const newSpanItem = document.createElement("span");
+  const newButtonItem = document.createElement("button");
+
+  newListItem.className = "todo-list-item";
+  newButtonItem.id = "deleteButton";
+  newButtonItem.innerHTML = `<i class="fas fa-trash"></i>`;
+
+  newSpanItem.appendChild(document.createTextNode(newTodo));
+  newListItem.appendChild(newSpanItem);
+  newListItem.appendChild(newButtonItem);
+
+  todoList.appendChild(newListItem);
+
+  addTodoText.value = "";
+}
+
+function addtodoToStorage(newTodo) {
+  let todos = gettodoFromStorage();
+
+  todos.push(newTodo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function gettodoFromStorage() {
+  let todos;
+
+  if(localStorage.getItem("todos") === null) {
+      todos = [];
+  }
+  else {
+      todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  return todos;
+}
+
+function loadAllToDosToUI() {
+  let todos = gettodoFromStorage();
+
+  todos.forEach(function(todo){
+    addTodoToUI(todo);
+  });
 }
 
 function deleteToDo(e) {
   if (e.target.className === "fas fa-trash") {
     e.target.parentElement.parentElement.remove();
-    //deleteToDoFromStorage(e.target.parentElement.parentElement.textContent)
+    deleteToDoFromStorage(e.target.parentElement.parentElement.textContent)
   }
+}
+
+function deleteToDoFromStorage(deletetodo) {
+  let todos = gettodoFromStorage();
+
+  todos.forEach(function (todo, index) {
+      if(todo === deletetodo) {
+          todos.splice(index,1);
+      }
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 // When the user clicks on the button, open the modal
